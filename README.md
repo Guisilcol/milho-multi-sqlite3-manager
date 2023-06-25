f9ab81b951be5757c7078c6c713223a6ed41e322
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2023-06-25 14:45:15 -0300

    Commit

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ The inspiration for creating this module came from the ease of working with Spar
 
 If you want an integrated environment on your machine, create an environment variable called "MULTISQLITE3MANAGER_FOLDER_PATH" with the directory of your folder. You will need to make sure that all files in this folder are SQLITE3 databases.
 
+When "to_dataframe" is used, the result is a Pandas DataFrame. The query is previously parsed to map all the databases used in the SQL. Then the module create a sqlalchemy connection and attach that databases to the connection. After that, the query is executed and the result is a Pandas DataFrame. 
+
 ## Code Samples
 
 ```python
--- multisqlite3manager/db_manager.py
+++ multisqlite3manager/db_manager.py
@@ -1,63 +1,185 @@
 import sqlalchemy
+import dataclasses
+import sqlglot
+import os 
+import glob
+import pandas
+import typing
 
 MULTISQLITE3MANAGER_FOLDER_PATH_KEY = "MULTISQLITE3MANAGER_FOLDER_PATH"
+MULTISQLITE3MANAGER_FOLDER_PATH_DEFAULT = os.getenv(MULTISQLITE3MANAGER_FOLDER_PATH_KEY)
 
-def get_connection(folder_path: str = None):
+@dataclasses.dataclass
+class _Database:
     """
-        Get a connection to a sqlite3 database file. Attach all other sqlite3 database files in the same folder.
-        :param folder_path: The folder path where the sqlite3 database files are located. If None, the environment variable MULTISQLITE3MANAGER_FOLDER_PATH will be used.
-        :return: A connection to the sqlite3 database files.
+        A class to represent a database.
+        :param db_name: The database name.
+        :param filename: The database filename.
     """
-    
-    from os import getenv
-    from sqlalchemy import create_engine, text
-    from glob import glob
-    from os.path import basename, splitext
+    db_name: str
+    filename: str
 
-    if folder_path == None and getenv(MULTISQLITE3MANAGER_FOLDER_PATH_KEY) == None:
-        raise Exception(f"You must provide a folder path or set the environment variable {MULTISQLITE3MANAGER_FOLDER_PATH_KEY}")
-    
-    folder = folder_path if folder_path is not None else getenv(MULTISQLITE3MANAGER_FOLDER_PATH_KEY)
+@dataclasses.dataclass
+class StorageEnvManager:
+    """
+        A class to represent a storage environment manager.
+        :param databases: A list of _Database objects.
+        :param database_folder_path: The path of the folder that stores the database files
+    """
+    databases: list[_Database]
+    database_folder_path: str
 
-    files = glob(f'{folder}/*')
+def _create_storage_env_manager(database_folder_path: str) -> StorageEnvManager:
+    """
+        Create a StorageEnvManager object.
+        :param database_folder_path: The folder path where the sqlite3 database files are located.
+        :return: A StorageEnvManager object.
+    """
 
-    first_db = None
+    if not os.path.isdir(database_folder_path):
+        raise NotADirectoryError(f"The folder path {database_folder_path} is not a directory.")
+
+    files = glob.glob(f'{database_folder_path}/*') 
 
     if len(files) == 0:
-        first_db = f"{folder}/main.db"
-    else: 
-        first_db = files.pop(0)
+        raise FileNotFoundError(f"The folder path {database_folder_path} does not contain any sqlite3 database file.")
 
-    connection = create_engine(f'sqlite:///{first_db}').connect()
+    databases = []
 
     for file in files:
-        db_name, _ = splitext(basename(file))
-        connection.execute(text(f"ATTACH DATABASE '{file}' AS {db_name}"))
+        db_name, _ = os.path.splitext(os.path.basename(file))
+        databases.append(_Database(db_name, file))
 
-    return connection
+    return StorageEnvManager(databases, database_folder_path)
 
-def print_databases(connection: sqlalchemy.engine.base.Connection):
+def _create_connection_with_attached_databases(databases: typing.List[_Database]) -> sqlalchemy.engine.base.Engine:
     """
-        Print all databases attached to the connection.
-        :param connection: The connection to the sqlite3 database files.
+        Create a connection with attached databases.
+        :param databases: A list of _Database objects.
+        :return: A sqlalchemy.engine.base.Engine object.
+    """
+
+    from sqlalchemy import text
+    
+    if len(databases) == 0:
+        raise ValueError("The databases list is empty.")
+
+    engine = sqlalchemy.create_engine('sqlite://', echo=False)
+
+    with engine.connect() as conn:
+        for db in databases:
+            conn.execute(text(f"ATTACH DATABASE '{db.filename}' AS {db.db_name};"))
+
+        return engine
+
+def _get_databases_name_from_sql_stmt(sql: str) -> list[str]:
+    """
+        Get all databases names from a sql statement.
+        :param sql: The sql statement.
+        :return: A list of databases names.
+    """
+
+    tables = [x for x in sqlglot.parse_one(sql).find_all(sqlglot.exp.Table)]
+    parts = [str(x).split('.') for x in tables]
+    dbs = [x[0] for x in parts if len(x) > 1]
+
+    return dbs
+
+def _validate_folder_path(folder_path: str):
+    """
+        Validate the folder path
+        :param folder_path: The folder path where the sqlite3 database files are located.
+    """
+    if folder_path == None and MULTISQLITE3MANAGER_FOLDER_PATH_DEFAULT == None:
+        return False
+    
+    return True
+
+def to_dataframe(sql: str, folder_path: str = MULTISQLITE3MANAGER_FOLDER_PATH_DEFAULT) -> pandas.DataFrame:
+    """
+        Execute a sql statement in all sqlite3 database files in the folder_path. Attach all databases in the SQL to the connection and execute the sql statement.
+        :param folder_path: A dataframe with the result of the sql statement.
+    """
+
+    if not _validate_folder_path(folder_path):
+        raise Exception(f"You must provide a folder path or set the environment variable {MULTISQLITE3MANAGER_FOLDER_PATH_KEY}")
+    
+    stmt_dbs = _get_databases_name_from_sql_stmt(sql)
+    env_manager = _create_storage_env_manager(folder_path)
+    storage_env_databases = [x.db_name for x in env_manager.databases]
+
+    if len(stmt_dbs) == 0:
+        raise ValueError("The sql statement does not contain any database name.")
+
+    for db in stmt_dbs:
+        if db not in storage_env_databases:
+            raise ValueError(f"The database {db} does not exist in the storage environment.")
+        
+    databases = [x for x in env_manager.databases if x.db_name in stmt_dbs]
+
+    engine = _create_connection_with_attached_databases(databases)
+
+    with engine.connect() as conn:
+        return pandas.read_sql(sql, conn)
+
+def create_connection(databases: typing.List[str], folder_path: str = MULTISQLITE3MANAGER_FOLDER_PATH_DEFAULT) -> sqlalchemy.engine.base.Connection:
+    """
+        Create a connection with attached databases.
+        :param databases: A list of databases names.
+        :param folder_path: The folder path where the sqlite3 database files are located.
+        :return: A sqlalchemy.engine.base.Connection object.
+    """
+
+    if not _validate_folder_path(folder_path):
+        raise Exception(f"You must provide a folder path or set the environment variable {MULTISQLITE3MANAGER_FOLDER_PATH_KEY}")
+
+    env_manager = _create_storage_env_manager(folder_path)
+    storage_env_databases = [x.db_name for x in env_manager.databases]
+
+    if len(databases) == 0:
+        raise ValueError("The databases list is empty.")
+
+    for db in databases:
+        if db not in storage_env_databases:
+            raise ValueError(f"The database {db} does not exist in the storage environment.")
+
+    dbs = [x for x in env_manager.databases if x.db_name in databases]
+
+    return _create_connection_with_attached_databases(dbs).connect()
+
+def print_databases(folder_path: str = MULTISQLITE3MANAGER_FOLDER_PATH_DEFAULT):
+    """
+        Print all databases in the folder_path.
+        :param folder_path: The folder path where the sqlite3 database files are located.
+    """
+
+    if not _validate_folder_path(folder_path):
+        raise Exception(f"You must provide a folder path or set the environment variable {MULTISQLITE3MANAGER_FOLDER_PATH_KEY}")
+
+    env_manager = _create_storage_env_manager(folder_path)
+
+    for db in env_manager.databases:
+        print('-> ', db.db_name, ' - ', db.filename)
+
+def print_tables(database_name: str, folder_path: str = MULTISQLITE3MANAGER_FOLDER_PATH_DEFAULT):
+    """
+        Print all tables in the database.
+        :param database: The database name.
+        :param folder_path: The folder path where the sqlite3 database files are located.
     """
     from sqlalchemy import text
 
-    result = connection.execute(text("PRAGMA database_list"))
-    for row in result:
-        _, db_name, path = row
-        print('Nome do banco de dados: ', f"'{db_name}'", ' - Diretório: ', f"'{path}'")
+    if not _validate_folder_path(folder_path):
+        raise Exception(f"You must provide a folder path or set the environment variable {MULTISQLITE3MANAGER_FOLDER_PATH_KEY}")
 
-def print_tables(connection: sqlalchemy.engine.base.Connection):
-    """
-        Print all tables in all databases attached to the connection (db_name.table_name).
-        :param connection: The connection to the sqlite3 database files.
-    """
-    from sqlalchemy import text
+    env_manager = _create_storage_env_manager(folder_path)
 
-    result = connection.execute(sqlalchemy.text("PRAGMA database_list"))
-    for row in result:
-        db_name = row[1]
-        tables = connection.execute(text(f"SELECT name FROM {db_name}.sqlite_master WHERE type='table'"))
+    if database_name not in [x.db_name for x in env_manager.databases]:
+        raise ValueError(f"The database {database_name} does not exist in the storage environment.")
+
+    engine = _create_connection_with_attached_databases([x for x in env_manager.databases if x.db_name == database_name])
+
+    with engine.connect() as conn:
+        tables = conn.execute(text(f"SELECT name FROM {database_name}.sqlite_master WHERE type='table';"))
         for table in tables:
-            print(f"{db_name}.{table[0]}")
+            print('-> ', database_name, ' - ', table[0])
